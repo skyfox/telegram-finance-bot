10cfb2f5e31ff25abb9ebd781c22d2304b385f5f
Dima Timofeev <skyfox@users.noreply.github.com>
2020-05-10 20:36:27 -0700

    CircleCI config

--- tests/storage_tests.py
+++ tests/storage_tests.py
@@ -4,6 +4,7 @@ import os
 import unittest
 import shutil
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../bot')))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
 from bot import storage, ledger_pb2
